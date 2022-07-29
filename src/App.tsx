a6fa2557fcac381c900164ca856c4b46739993e3
Anastasiya Rabetskaya <nastia1362@gmail.com>
2022-07-29 19:30:26 +0300

    uncontrolled components

--- src/App.tsx
+++ src/App.tsx
@@ -9,23 +9,16 @@ import {UncontrolledRating} from "./components/UncontrolledRating/UncontrolledRa
 function App() {
     console.log('App rendering');
     return (
-        <div>
+        <div className='App'>
             <PageTitle title={'This is App component'}/>
             <PageTitle title={'My friends'}/>
             Article 1
             <Rating value={3}/>
-            {/*<Accordion titleValue={'Menu'} collapsed={true}/>
-            <Accordion titleValue={'Users'} collapsed={false}/>*/}
-            <UncontrolledAccordion titleValue={'Menu'}/>
-            <UncontrolledAccordion titleValue={'Users'}/>
+            <Accordion titleValue={'Users'} collapsed={false}/>
+
             Article 2
             <UncontrolledRating/>
-            {/*<Rating value={0}/>
-            <Rating value={1}/>
-            <Rating value={2}/>
-            <Rating value={3}/>
-            <Rating value={4}/>
-            <Rating value={5}/>*/}
+            <UncontrolledAccordion titleValue={'Menu'}/>
             <OnOff/>
         </div>
     );
--- src/components/UncontrolledRating/UncontrolledRating.tsx
+++ src/components/UncontrolledRating/UncontrolledRating.tsx
@@ -28,11 +28,11 @@ type StarPropsType = {
 
 function Star(props: StarPropsType) {
     console.log('Star rendering');
-    if (props.selected === true) {
-        return <span><b>star </b></span>
-    } else {
-        return <span>star </span>
-    }
 
+    return (
+        <>
+            {props.selected ? <span><b>star </b></span> : <span>star </span>}
+        </>
+    )
 
 }
