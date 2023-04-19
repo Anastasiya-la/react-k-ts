158c553d47c2651807ed1a0fcc094b06df6c0e43
Anastasiya Rabetskaya <nastia1362@gmail.com>
2023-04-20 00:11:55 +0300

    22-useEffect \ setTimeout \ setInterval

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -36,3 +36,39 @@ export const SimpleExample = () => {
         </div>
     )
 }
+
+export const SetTimeoutExample = () => {
+
+    const [counter, setCounter] = useState(1)
+    const [fake, setFake] = useState(1)
+
+    console.log('SetTimeoutExample')
+
+    /*   useEffect(() => {
+           setTimeout(() => {
+               console.log('setTimeOut')
+               document.title = counter.toString()
+           }, 1000)
+       }, [])
+   */
+    useEffect(() => {
+        setInterval(() => {
+            setCounter(state => state + 1)
+        }, 1000)
+    }, [])
+
+
+    /* setTimeout(() => {
+         console.log('setTimeOut')
+         document.title = counter.toString()
+     }, 1000)*/
+
+    return (
+        <div>
+            {/*<button onClick={() => setCounter(counter + 1)}>counter+</button>
+            <button onClick={() => setFake(fake + 1)}>fake+</button>*/}
+            Hello, counter: {counter} - fake :{fake}
+        </div>
+    )
+}
+
